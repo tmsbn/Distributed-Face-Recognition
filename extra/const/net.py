13fc8c00225e97f48610ac0276397f4036bd4e6e
Thomas <mailthomasb@gmail.com>
2018-04-18 14:56:22 -0400

    Cleanup

--- extra/const/net.py
+++
@@ -1,7 +0,0 @@
-PATH_TO_PARENT = 'C:\\Users\\Ruzan\\Documents\\Homework Spring 18\\DS\\DDNN\\Flask\\'
-PATH_TO_IMAGES = PATH_TO_PARENT + 'images\\'
-
-PORT = '5000'
-
-# EDGE_IP = '172.17.0.2'
-EDGE_IP = '192.168.56.1'
